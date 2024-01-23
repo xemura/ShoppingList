8714d388e691440bda82f08076600b489d321145
Xenia <markg0s@mail.ru>
2024-01-23 14:42:07 +0300

    add domain layer

---
+++ app/src/main/java/com/xenia/shopinglist/domain/EditShopItemUseCase.kt
@@ -0,0 +1,7 @@
+package com.xenia.shopinglist.domain
+
+class EditShopItemUseCase(private val shopListRepository: ShopListRepository) {
+    fun editShopItem(shopItem: ShopItem) {
+        shopListRepository.editShopItem(shopItem)
+    }
+}
---
+++ app/src/main/java/com/xenia/shopinglist/domain/ShopListRepository.kt
@@ -0,0 +1,9 @@
+package com.xenia.shopinglist.domain
+
+interface ShopListRepository {
+    fun addShopItem(shopItem: ShopItem)
+    fun deleteShopItem(shopItem: ShopItem)
+    fun editShopItem(shopItem: ShopItem)
+    fun getShopItem(shopItemId: Int): ShopItem
+    fun getShopList(): List<ShopItem>
+}
