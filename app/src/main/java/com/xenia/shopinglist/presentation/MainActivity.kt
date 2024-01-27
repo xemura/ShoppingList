81dbe0cdf141cb3a9778ee3bf731f4c78ade0930
Xenia <markg0s@mail.ru>
2024-01-27 14:19:00 +0300

    add long listener to item

--- app/src/main/java/com/xenia/shopinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/xenia/shopinglist/presentation/MainActivity.kt
@@ -4,18 +4,52 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.xenia.shopinglist.R
+import com.xenia.shopinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: ShopListAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setUpRecyclerView()
+
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-        viewModel.shopList.observe(this) {
-            Log.d("MainActivity", it.toString())
+        viewModel.shopList.observe(this) { it ->
+            adapter.shopList = it
+        }
+    }
+
+    private fun setUpRecyclerView() {
+        recyclerView = findViewById(R.id.rv_shop_list)
+        adapter = ShopListAdapter()
+        recyclerView.adapter = adapter
+
+        recyclerView.recycledViewPool.setMaxRecycledViews(
+            ShopListAdapter.VIEW_TYPE_ENABLED,
+            ShopListAdapter.MAX_POOL_SIZE
+        )
+        recyclerView.recycledViewPool.setMaxRecycledViews(
+            ShopListAdapter.VIEW_TYPE_DISABLED,
+            ShopListAdapter.MAX_POOL_SIZE
+        )
+
+        adapter.onShopItemLongClickListener =  {
+                viewModel.changeEnableState(it)
         }
     }
 }
+
+
+
+
+
+
+
+
+
