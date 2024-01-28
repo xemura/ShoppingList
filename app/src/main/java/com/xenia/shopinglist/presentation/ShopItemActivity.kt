b26c1126d2e7a10244e655e7191d289acb58e4a0
Xenia <markg0s@mail.ru>
2024-01-28 18:21:47 +0300

    add new layer for edit and add shop item

---
+++ app/src/main/java/com/xenia/shopinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.xenia.shopinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.xenia.shopinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
